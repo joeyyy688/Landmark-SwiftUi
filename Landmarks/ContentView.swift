24931445b2528a5ae32f7da6a3c53a357955fd9c
joeyyy688 <edinjoey@gmail.com>
2023-06-11 20:44:05 +0000

    initial commit

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            MapView().frame(height: 300)
+            
+            CircleImage().offset(y: -130).padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock").font(.title).foregroundColor(.purple)
+                HStack() {
+                    Text("Joshua Tree")
+                    Spacer()
+                    Text("California Park Tree")
+                }
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }.padding()
+            
+            Spacer()
+            
+            
         }
-        .padding()
     }
 }
 
